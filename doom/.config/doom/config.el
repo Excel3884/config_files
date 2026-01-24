77fb1c944a270423d321aa072daba5563f5e46eb
Excel3884 <angelosdimoglis@gmail.com>
2026-01-24 14:02:10 +0100

    Org mode: adjusted headings' size

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -107,10 +107,10 @@
 'doom-one
 '(org-level-5 :inherit outline-3 :height 1.0)
 '(org-level-4 :inherit outline-3 :height 1.0)
-'(org-level-3 :inherit outline-3 :height 1.1)
-'(org-level-2 :inherit outline-2 :height 1.2)
-'(org-level-1 :inherit outline-1 :height 1.3)
-'(org-document-title  :height 1.5 :bold t :underline nil))
+'(org-level-3 :inherit outline-3 :height 1.0)
+'(org-level-2 :inherit outline-2 :height 1.1)
+'(org-level-1 :inherit outline-1 :height 1.2)
+'(org-document-title  :height 1.3 :bold t :underline nil))
 
 (setq org-journal-date-prefix "#+TITLE: "
       org-journal-time-prefix ""
@@ -123,9 +123,9 @@
 ;; Markdown settings (inspired by DT's config)
 (custom-set-faces
  '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold :family "variable-pitch"))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.3))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.2))))
- '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.1))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :height 1.2))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :height 1.1))))
+ '(markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.0))))
  '(markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.0))))
  '(markdown-header-face-5 ((t (:inherit markdown-header-face :height 1.0)))))
 
